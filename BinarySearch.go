9a1a9511eec0c754a3a230d61d4582dadccf1cfd
Alexander Kryukov <112829166+LafiTafi1@users.noreply.github.com>
2023-05-16 15:39:52 +0300

    Add files via upload

---
+++ BinarySearch.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{1, 2, 4, 5, 12, 145, 234, 1013, 12342}
+	fmt.Println(binarySearch(nums, 1013))
+}
+
+func binarySearch(slice []int, n int) bool {
+	first := 0
+	last := len(slice) - 1
+	for last >= first {
+		mid := int((first + last) / 2)
+		if slice[mid] == n {
+			return true
+		} else {
+			if n > slice[mid] {
+				first = mid + 1
+			} else {
+				last = mid - 1
+			}
+		}
+	}
+	return false
+}
